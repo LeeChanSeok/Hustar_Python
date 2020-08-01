982d3b1ad6f9d01c9f0fab5c690ba830af98334e
LeeChanSeok <ckstjr93@naver.com>
2020-08-01 21:45:54 +0900

    input fileName on console

--- project1_1.py
+++ project1_1.py
@@ -15,3 +15,34 @@
 명시되어 있지 않은 세부사항에 대해서 처리한 방법, 이유 등을 보고서에 기록하도록 한다.
 '''
 
+
+
+'''
+(설명)
+ 프로그램 실행은 다음과 같이 한다. (실행예시에 밑줄로 표시된 문자는 사용자 입력에 해당)
+     리눅스의 경우
+    $ python project1.py students.txt
+     윈도우의 경우
+    C:\>python project1.py students.txt
+ 파일명을 입력하지 않을 경우, default로 “students.txt”로부터 데이터를 읽는다.
+ 파일명이 입력될 경우, 입력된 파일로부터 데이터를 읽는다.
+ 파일명에는 공백이 없다고 가정한다. (즉, 공백이 있는 파일명의 입력에 대해서는 고려하지 않는다.
+'''
+
+import sys
+
+def main():
+    # $ python project1.py students.txt
+    # project1.py == sys.argv[0], students.txt == sys.argv[1]
+
+    try:
+        fileName = sys.argv[1]
+    except:
+        fileName = "students.txt"
+
+    print(fileName)
+
+# main함수가 있으면 main함수를 실행한다.
+if __name__ == "__main__":
+    main()
+
